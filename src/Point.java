0134e588133ab3830bc74c39f87d372267f114d8
Karishma <tikkysood@yahoo.com>
2014-11-22 22:38:56 -0700

    Karishma UpdatePoint.java
    Sorry its late. i didnt understand how to do it, but i get it now

--- src/Point.java
+++ src/Point.java
@@ -11,6 +11,31 @@ public class Point
   {
     return y;
   }
-  public double distanceFromOrigin
+  public double distanceFromOrigin()
   {
-}
+    double disorigin=Math.sqrt(x*x+y*y);
+    return disorigin;
+  }
+  public double distance (Point p)
+  {
+    double dis=Math.sqrt(((x-p.x)*(x-p.x))+((y-p.y)*(y-p.y)));
+    return dis;
+  }
+   public String toString()
+   {
+     return "("+x+","+y+")";
+   }
+   public void setX(int x1)
+   {
+     x=x1;
+   }
+   public void setY(int y2)
+   {
+     y=y2;
+   }
+   public void translate (int x1, int y1)
+   {
+     x+=x1;
+     y+=y1;
+   }
+ }
